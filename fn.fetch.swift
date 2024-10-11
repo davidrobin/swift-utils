e7abf7d654529f723bb4f7ebff75d6155a833d42
David ROBIN <darobin@gmail.com>
2024-10-11 10:15:35 +0200

    Create fn.fetch.swift

---
+++ fn.fetch.swift
@@ -0,0 +1,75 @@
+import Foundation
+
+enum fetchError: Error {
+  case invalidStatusCode
+  case invalidMimeType
+  case invalidBody
+  case unknownType
+  case unknownMethod
+  case urlNotSet
+}
+
+func fetch (
+  url: String, type: String, method: String = "GET", headers: [String: String] = [:], body: [String: Any] = [:]
+) async throws -> (
+  statusCode: Int, mimeType: String, type: String, data: Data
+) {
+  
+  let acceptedStatusCodes: [Int] = [200, 201, 202]
+  let acceptedMethods: [String] = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+  let mimeTypes: [String: String] = [
+    "text": "text/plain",
+    "html": "text/html",
+    "json": "application/json"
+  ]
+  
+  if !acceptedMethods.contains(method) {
+    throw fetchError.unknownMethod
+  }
+  
+  guard let mimeType: String = mimeTypes[type] else {
+    throw fetchError.unknownType
+  }
+  
+  guard let url = URL(string: url) else {
+    throw fetchError.urlNotSet
+  }
+  
+  var request = URLRequest(url: url)
+  
+  request.httpMethod = method
+  
+  request.setValue(mimeType, forHTTPHeaderField: "Accept")
+  
+  for (header, value) in headers {
+    request.setValue(value, forHTTPHeaderField: header)
+  }
+  
+  if (body.isEmpty == false && type == "json") {
+    guard let jsonBody = try? JSONSerialization.data(withJSONObject: body) else {
+      throw fetchError.invalidBody
+    }
+    
+    request.setValue(mimeType, forHTTPHeaderField: "Content-Type")
+    
+    request.httpBody = jsonBody
+  }
+
+  let (data, response) = try await URLSession.shared.data(for: request)
+  
+  guard let httpResponse = response as? HTTPURLResponse,
+  acceptedStatusCodes.contains(httpResponse.statusCode) else {
+    throw fetchError.invalidStatusCode
+  }
+  
+  if httpResponse.mimeType != mimeType {
+    throw fetchError.invalidMimeType
+  }
+  
+  return (
+    statusCode: httpResponse.statusCode,
+    mimeType: httpResponse.mimeType!,
+    type: type,
+    data: data
+  )
+}
